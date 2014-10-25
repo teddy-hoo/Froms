daf6472fcdd96af2dc96077054df5c644f53680f
teddy-hoo <hulingchuan@hotmail.com>
2014-10-25 20:55:34 +0800

    init project

---
+++ js/config.js
@@ -0,0 +1,4 @@
+var TAGS = {
+	"Email Address": '<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">',
+	Dropdown: "Dropdown"
+};
---
+++ js/forms.js
@@ -0,0 +1,60 @@
+var formsApp = angular.module('formsApp', []);
+
+formsApp.controller('FormsCtrl', function($scope) {
+    $scope.tags = TAGS;
+    $scope.tagNames = Object.keys($scope.tags);
+    $scope.editingTitle = false;
+    $scope.eventTitle = "Click here to input a title";
+
+    $scope.editTitle = function(){
+    	$scope.editingTitle = true;
+    };
+
+    $scope.finishTitle = function(event){
+    	$scope.editingTitle = event.which == 13 ? false : true;
+    };
+});
+
+formsApp.directive('ngDraggable', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+
+            angular.element(element).attr("draggable", true);
+
+            element.bind("dragstart", function(event) {
+                event.dataTransfer.setData("tagName", element.text());
+            });
+
+            element.bind("dragend", function(event) {})
+        }
+    }
+});
+
+formsApp.directive('ngDropable', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+
+            element.bind("dragenter", function(event) {
+                element.addClass("drag-enter");
+            });
+
+            element.bind("dragover", function(event) {
+                event.preventDefault();
+            });
+
+            element.bind("dragleave", function(event) {
+                element.removeClass("drag-enter");
+            });
+
+            element.bind("drop", function(event) {
+                event.preventDefault();
+                element.removeClass("drag-enter");
+                var tagName = event.dataTransfer.getData("tagName").trim();
+                element.append(scope.tags[tagName]);
+            });
+
+        }
+    }
+});
